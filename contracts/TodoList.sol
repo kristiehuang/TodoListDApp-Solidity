2e22942802253d4e6965333bdd33e450b229362e
Kristie Huang <kristiehuang01@gmail.com>
2021-09-24 23:28:29 -0700

    Add createTask fxn

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,26 @@ pragma solidity ^0.5.0;
 // my truffle uses solidity 0.5.0, but best to dev on latest versions of solidity
 
 contract TodoList {
-    uint public taskCount = 0; // state variable; scoped for entire smart contract
-    // public keyword; solidity auto creates a taskCount() fxn to read value outside of contract
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // State Variables /////////////////////////
+    // scoped for entire smart contract
+    // public keyword: Solidity auto creates a taskCount() fxn to read value outside of contract
+    uint public taskCount = 0; 
+    mapping(uint => Task) public tasks; // basically a dict
+
+    // Contructor /////////////////////////
+    constructor() public {
+        createTask("My first task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
