ad1c414aaf802e15d727596d0b34d3ff20751cc1
Kristie Huang <kristiehuang01@gmail.com>
2021-09-24 23:34:40 -0700

    Add event for TaskCreated

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -9,11 +9,19 @@ contract TodoList {
         bool completed;
     }
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     // State Variables /////////////////////////
     // scoped for entire smart contract
     // public keyword: Solidity auto creates a taskCount() fxn to read value outside of contract
     uint public taskCount = 0; 
     mapping(uint => Task) public tasks; // basically a dict
+
+
 
     // Contructor /////////////////////////
     constructor() public {
@@ -23,5 +31,10 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
+    // create event to track every time something happens in code, so that
+    // external listeners can listen to these events
+
+
 }
